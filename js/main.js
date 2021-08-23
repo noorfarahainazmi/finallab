6ae9f3ee01839c16f3c7de7f20376f4fd4b69613
Noor Farah Ain Azmi <noorfarahainazmi@gmail.com>
2021-08-23 12:15:41 +0800

    final__project
    here is my final project but still not done yet

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var popupDiv = document.getElementById("popup__bg");
+
+function openPopupMenu() {
+    popupDiv.style.display = "block";
+}
+function closePopupMenu(){
+    popupDiv.style.display = "none";
+}
